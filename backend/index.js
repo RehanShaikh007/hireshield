1d64c34483fc25de9312259e9e715a64dd9bffd6
Rehan Shaikh <rehanmukhtarshaikh7@gmail.com>
2025-09-01 21:19:32 +0530

    changes in package file

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,27 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from './routes/auth.js';
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true, 
+  }));
 app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 // Health check endpoint
 app.get('/api/health', (req, res) => {
